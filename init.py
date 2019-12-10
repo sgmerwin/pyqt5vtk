7c7e9804021af3c5b9259f3cbdcbd20847ae964a
sgmerwin <stevegmerwin@gmail.com>
2019-12-10 13:46:58 -0700

    Add files via upload

--- init.py
+++ init.py
@@ -5,6 +5,26 @@ from vtk.qt.QVTKRenderWindowInteractor import QVTKRenderWindowInteractor
 from PyQt5.QtWidgets import QMainWindow, QApplication, QDialog, QFileDialog
 from foo import Ui_MainWindow
 from PyQt5 import Qt
+import threading
+import time
+
+class vtkTimerCallback():
+    def __init__(self):
+        self.timer_count = 0
+        self.count = 0
+
+    def execute(self, obj, event):
+        self.actor.SetPosition(self.timer_count, self.timer_count, 0)
+        iren = obj
+        iren.GetRenderWindow().Render()
+        if self.count == 0:
+            self.timer_count += 1
+        if self.timer_count == 200:
+            self.count = 1
+        if self.count == 1:
+            self.timer_count -= 1
+        if self.timer_count == -200:
+            self.count = 0
 
 class MainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self, parent=None):
@@ -18,6 +38,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.vtkWidget = QVTKRenderWindowInteractor(self.frame)
         self.vl = Qt.QVBoxLayout()
         self.vl.addWidget(self.vtkWidget)
+
 
         self.ren = vtk.vtkRenderer()
         self.vtkWidget.GetRenderWindow().AddRenderer(self.ren)
@@ -37,12 +58,31 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         # Create a mapper
         mapper = vtk.vtkPolyDataMapper()
         mapper.SetInputConnection(reader.GetOutputPort())
+        mapper2 = vtk.vtkPolyDataMapper()
+        mapper2.SetInputConnection(source.GetOutputPort())
+
 
         # Create an actor
         actor = vtk.vtkActor()
         actor.SetMapper(mapper)
+        actor2 = vtk.vtkActor()
+        actor2.SetMapper(mapper2)
+        actor2.SetPosition((100, 100, 100))
+
+
+        # def runA():
+        #     while True:
+        #         actor2.SetPosition((-100,-100,0))
+        #         time.sleep(4)
+        #         actor2.SetPosition((100, 100, 100))
+        #         time.sleep(4)
+        #
+        # t1 = threading.Thread(target = runA)
+        # t1.setDaemon(True)
+        # t1.start()
 
         self.ren.AddActor(actor)
+        self.ren.AddActor(actor2)
 
         self.ren.ResetCamera()
 
@@ -51,6 +91,14 @@ class MainWindow(QMainWindow, Ui_MainWindow):
 
         self.show()
         self.iren.Initialize()
+        #put timer event here
+
+        cb = vtkTimerCallback()
+        cb.actor = actor2
+        self.vtkWidget.AddObserver('TimerEvent', cb.execute)
+        self.vtkWidget.CreateRepeatingTimer(1)
+
+
         self.iren.Start()
 
 if __name__ == "__main__":
