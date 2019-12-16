849e1e142f6d18e1e178538eac3fb7065a6fac66
sgmerwin <stevegmerwin@gmail.com>
2019-12-16 14:42:05 -0700

    Add files via upload

--- init.py
+++ init.py
@@ -24,9 +24,10 @@ class vtkTimerCallback():
         self.timerVar2 = 0
         self.timerVar1 = 0
 
+
     def execute(self, obj, event):
         if self.timerVar1 == 0:
-            self.actor.SetPosition(self.timer_count, self.timer_count, 0)
+            self.actor.SetPosition(0, self.timer_count, 420)
             if self.count == 0:
                 self.timer_count += 1
 
@@ -43,7 +44,7 @@ class vtkTimerCallback():
 
 
         if self.timerVar1 == 1:
-            self.actor.SetPosition(self.timer_count, self.timer_count, 0)
+            self.actor.SetPosition(0, self.timer_count, 420)
             iren = obj
             iren.Initialize()
 
@@ -66,6 +67,50 @@ class vtkTimerCallback():
 
         if self.timerVar2 == 1:
             self.actor.RotateY(0)
+            iren = obj
+            iren.Initialize()
+
+    def execute3(self, obj, event):
+        if self.timerVar1 == 0:
+            self.actor.SetPosition(760, self.timer_count, 220)
+            if self.count == 0:
+                self.timer_count += 1
+
+            if self.timer_count > 200:
+                self.count = 1
+            if self.count == 1:
+                self.timer_count -= 1
+
+            if self.timer_count < -200:
+                self.count = 0
+
+            iren = obj
+            iren.Initialize()
+
+        if self.timerVar1 == 1:
+            self.actor.SetPosition(760, self.timer_count, 220)
+            iren = obj
+            iren.Initialize()
+
+    def execute4(self, obj, event):
+        if self.timerVar1 == 0:
+            self.actor.SetPosition(-760, self.timer_count, 220)
+            if self.count == 0:
+                self.timer_count += 1
+
+            if self.timer_count > 200:
+                self.count = 1
+            if self.count == 1:
+                self.timer_count -= 1
+
+            if self.timer_count < -200:
+                self.count = 0
+
+            iren = obj
+            iren.Initialize()
+
+        if self.timerVar1 == 1:
+            self.actor.SetPosition(-760, self.timer_count, 220)
             iren = obj
             iren.Initialize()
 
@@ -92,9 +137,29 @@ class MainWindow(QMainWindow, Ui4_MainWindow):
         self.iren.GetInteractorStyle().SetCurrentStyleToTrackballCamera()
 
         # Create source
-        source = vtk.vtkSphereSource()
+        # source = vtk.vtkSphereSource()
+        # source.SetCenter(0, 0, 0)
+        # source.SetRadius(5.0)
+
+        source = vtk.vtkCubeSource()
+        source.SetXLength(1500)
+        source.SetYLength(1500)
+        source.SetZLength(100)
         source.SetCenter(0, 0, 0)
-        source.SetRadius(5.0)
+
+        source2 = vtk.vtkCubeSource()
+        source2.SetXLength(25)
+        source2.SetYLength(200)
+        source2.SetZLength(550)
+        source2.SetCenter(0, 0, 0)
+
+        source3 = vtk.vtkCubeSource()
+        source3.SetXLength(1500)
+        source3.SetYLength(25)
+        source3.SetZLength(150)
+        source2.SetCenter(0, 0, 0)
+
+
 
         filename = "/Users/steve/PycharmProjects/vtktest/CylinderHead-stl/CylinderHead-binary.stl"
         reader = vtk.vtkSTLReader()
@@ -102,20 +167,44 @@ class MainWindow(QMainWindow, Ui4_MainWindow):
         reader.Update()
 
         # Create a mapper
-        mapper = vtk.vtkPolyDataMapper()
-        mapper.SetInputConnection(reader.GetOutputPort())
-        mapper2 = vtk.vtkPolyDataMapper()
-        mapper2.SetInputConnection(source.GetOutputPort())
+        #mapperSTL = vtk.vtkPolyDataMapper()
+        #mapperSTL.SetInputConnection(reader.GetOutputPort())
+        mapperBase = vtk.vtkPolyDataMapper()
+        mapperBase.SetInputConnection(source.GetOutputPort())
+        mapperBase2 = vtk.vtkPolyDataMapper()
+        mapperBase2.SetInputConnection(source2.GetOutputPort())
+        mapperBase3 = vtk.vtkPolyDataMapper()
+        mapperBase3.SetInputConnection(source2.GetOutputPort())
+        mapperBase4 = vtk.vtkPolyDataMapper()
+        mapperBase4.SetInputConnection(source3.GetOutputPort())
 
         # Create an actor
-        STLactor = vtk.vtkActor()
-        STLactor.SetMapper(mapper)
-        sphereactor = vtk.vtkActor()
-        sphereactor.SetMapper(mapper2)
-        sphereactor.SetPosition((100, 100, 100))
+        #STLactor = vtk.vtkActor()
+        #STLactor.SetMapper(mapperSTL)
+        BaseActor = vtk.vtkActor()
+        BaseActor.SetMapper(mapperBase)
+        BaseActor.SetPosition((0, 0, 0))
+        BaseActor.GetProperty().SetColor(1.0,0,0)
+        #STLactor.SetScale(5,5,5)
+        BaseActor2 = vtk.vtkActor()
+        BaseActor2.SetMapper(mapperBase2)
+        BaseActor2.GetProperty().SetColor(0, 0, 1.0)
+        BaseActor2.SetPosition(760,0,180)
+        BaseActor3 = vtk.vtkActor()
+        BaseActor3.SetMapper(mapperBase3)
+        BaseActor3.GetProperty().SetColor(0, 0, 1.0)
+        BaseActor3.SetPosition(-760, 0, 180)
+        BaseActor4 = vtk.vtkActor()
+        BaseActor4.SetMapper(mapperBase4)
+        BaseActor4.GetProperty().SetColor(0, 0, 1.0)
+        BaseActor4.SetPosition(0, 0, 380)
 
-        self.ren.AddActor(STLactor)
-        self.ren.AddActor(sphereactor)
+        #self.ren.AddActor(STLactor)
+        self.ren.AddActor(BaseActor)
+        self.ren.AddActor(BaseActor2)
+        self.ren.AddActor(BaseActor3)
+        self.ren.AddActor(BaseActor4)
+
 
         self.ren.ResetCamera()
 
@@ -126,10 +215,14 @@ class MainWindow(QMainWindow, Ui4_MainWindow):
         self.iren.Initialize()
         # put timer event here
 
-        cbSphere = vtkTimerCallback()
-        cbSphere.actor = sphereactor
-        cbSTL = vtkTimerCallback()
-        cbSTL.actor = STLactor
+        cbBase2 = vtkTimerCallback()
+        cbBase3 = vtkTimerCallback()
+        cbBase4 = vtkTimerCallback()
+        cbBase2.actor = BaseActor2
+        cbBase3.actor = BaseActor3
+        cbBase4.actor = BaseActor4
+        #cbSTL = vtkTimerCallback()
+        #cbSTL.actor = STLactor
 
         def getfiles():
             try:
@@ -139,8 +232,8 @@ class MainWindow(QMainWindow, Ui4_MainWindow):
                 str = file[0]
                 reader.SetFileName(str)
                 reader.Update()
-                mapper.SetInputConnection(reader.GetOutputPort())
-                STLactor.SetMapper(mapper)
+                #mapper.SetInputConnection(reader.GetOutputPort())
+                #STLactor.SetMapper(mapper)
             except Exception as e:
                 print("Exception in method")
                 print(e)
@@ -152,15 +245,22 @@ class MainWindow(QMainWindow, Ui4_MainWindow):
             cbSTL.timerVar2 = 0
 
         def stopL():
-            cbSphere.timerVar1 = 1
+            cbBase2.timerVar1 = 1
+            cbBase3.timerVar1 = 1
+            cbBase4.timerVar1 = 1
 
         def startL():
-            cbSphere.timerVar1 = 0
+            cbBase2.timerVar1 = 0
+            cbBase3.timerVar1 = 0
+            cbBase4.timerVar1 = 0
 
-        self.vtkWidget.AddObserver('TimerEvent', cbSTL.execute2)
-        self.vtkWidget.AddObserver('TimerEvent', cbSphere.execute)
-        self.stopRotationButton.clicked.connect(stopR)
-        self.startRotationButton.clicked.connect(startR)
+        #self.vtkWidget.AddObserver('TimerEvent', cbSTL.execute2)
+        self.vtkWidget.AddObserver('TimerEvent', cbBase2.execute3)
+        self.vtkWidget.AddObserver('TimerEvent', cbBase3.execute4)
+        self.vtkWidget.AddObserver('TimerEvent', cbBase4.execute)
+
+        #self.stopRotationButton.clicked.connect(stopR)
+        #self.startRotationButton.clicked.connect(startR)
         try:
             self.STLButton.clicked.connect(getfiles)
         except Exception as ex:
